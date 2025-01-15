220253909ba3924fb1022d25c1d04841bad35d6c
Dan Galvin <dan.galvin@soundunited.com>
2025-01-15 15:42:18 +0000

    change version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject audiogum/dynamo-embedded-clj "3.0.2-SNAPSHOT"
+(defproject audiogum/dynamo-embedded-clj "3.1.0-SNAPSHOT"
   :description "Embedded dynamodb for clojure"
   :url "https://github.com/bwgroupltd/dynamo-embedded-clj"
   :license {:name "Eclipse Public License"
